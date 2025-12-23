61bf169e191c8931cc79e79d25b2a53801f334e6
nisanuryildizeng <nisanuryildiz228@gmail.com>
2025-12-23 18:37:28 +0300

    LibraryManager sınıfı eklendi,ArrayList ile kitap yonetimi sağlandı.

---
+++ src/kutuphane/model/LibraryManager.java
@@ -0,0 +1,9 @@
+package kutuphane.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class LibraryManager {
+
+}
--- src/kutuphane/model/Robot.java
+++ src/kutuphane/model/Robot.java
@@ -1,5 +1,15 @@
 package kutuphane.model;
 
-public class Robot {
+public class Robot extends Calisan{
+public Robot(int id,String ad,String soyad) {
+	super(id,ad,soyad);
+}
+
+@Override
+public void calis() {
+	System.out.println(getAd()+"isimli robot kütüphane raflarini düzenliyor.");
+}
+
+
 
 }
